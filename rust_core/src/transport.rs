e21dde081f2e6106ceb22ded1c3d9444998e057a
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-02-08 10:18:59 +0300

    Implement in-memory RTT channel queue

--- rust_core/src/transport.rs
+++ rust_core/src/transport.rs
@@ -1,6 +1,8 @@
 use pyo3::exceptions::PyNotImplementedError;
+use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use std::collections::HashMap;
+use std::collections::VecDeque;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Mutex;
 
@@ -47,6 +49,7 @@ pub struct TransportHandle {
     config: TransportConfig,
     next_id: AtomicU64,
     channels: Mutex<HashMap<u64, ChannelKind>>,
+    queues: Mutex<HashMap<u64, VecDeque<Vec<u8>>>>,
 }
 
 #[pymethods]
@@ -57,6 +60,7 @@ impl TransportHandle {
             config,
             next_id: AtomicU64::new(1),
             channels: Mutex::new(HashMap::new()),
+            queues: Mutex::new(HashMap::new()),
         }
     }
 
@@ -82,19 +86,53 @@ impl TransportHandle {
             return Err(PyNotImplementedError::new_err("max channels exceeded"));
         }
         channels.insert(channel_id, channel_kind);
+        let mut queues = self
+            .queues
+            .lock()
+            .map_err(|_| PyNotImplementedError::new_err("queue registry poisoned"))?;
+        queues.insert(channel_id, VecDeque::new());
         Ok(channel_id)
     }
 
-    fn send(&self, _channel: u64, _payload: &[u8]) -> PyResult<()> {
-        Err(PyNotImplementedError::new_err(
-            "transport send not implemented",
-        ))
+    fn send(&self, channel: u64, payload: &[u8]) -> PyResult<()> {
+        let channels = self
+            .channels
+            .lock()
+            .map_err(|_| PyNotImplementedError::new_err("channel registry poisoned"))?;
+        if !channels.contains_key(&channel) {
+            return Err(PyValueError::new_err("unknown channel"));
+        }
+        drop(channels);
+        let mut queues = self
+            .queues
+            .lock()
+            .map_err(|_| PyNotImplementedError::new_err("queue registry poisoned"))?;
+        if let Some(queue) = queues.get_mut(&channel) {
+            queue.push_back(payload.to_vec());
+            Ok(())
+        } else {
+            Err(PyValueError::new_err("channel queue missing"))
+        }
     }
 
-    fn receive(&self, _channel: u64) -> PyResult<Vec<u8>> {
-        Err(PyNotImplementedError::new_err(
-            "transport receive not implemented",
-        ))
+    fn receive(&self, channel: u64) -> PyResult<Vec<u8>> {
+        let channels = self
+            .channels
+            .lock()
+            .map_err(|_| PyNotImplementedError::new_err("channel registry poisoned"))?;
+        if !channels.contains_key(&channel) {
+            return Err(PyValueError::new_err("unknown channel"));
+        }
+        drop(channels);
+        let mut queues = self
+            .queues
+            .lock()
+            .map_err(|_| PyNotImplementedError::new_err("queue registry poisoned"))?;
+        if let Some(queue) = queues.get_mut(&channel) {
+            Ok(queue.pop_front().unwrap_or_default())
+        } else {
+            Err(PyValueError::new_err("channel queue missing"))
+        }
     }
 
     fn close_channel(&self, channel: u64) -> PyResult<()> {
@@ -103,6 +141,11 @@ impl TransportHandle {
             .lock()
             .map_err(|_| PyNotImplementedError::new_err("channel registry poisoned"))?;
         channels.remove(&channel);
+        let mut queues = self
+            .queues
+            .lock()
+            .map_err(|_| PyNotImplementedError::new_err("queue registry poisoned"))?;
+        queues.remove(&channel);
         Ok(())
     }
 }
