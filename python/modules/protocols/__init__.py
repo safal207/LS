1742c0d9c40502c802b1216d848f83d1b91f4c13
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-02-08 10:50:45 +0300

    Add trust FSM for protocol peers

--- python/modules/protocols/__init__.py
+++ python/modules/protocols/__init__.py
@@ -3,6 +3,7 @@
 from .cip import CIP_VERSION, CipIdentity, CipState, build_envelope as build_cip_envelope
 from .hcp import HCP_VERSION, HcpHumanState, HcpIdentity, build_envelope as build_hcp_envelope
 from .lip import LIP_VERSION, LipIdentity, LipSource, build_envelope as build_lip_envelope
+from .trust import TrustFSM, TrustState, TrustTransition
 
 __all__ = [
     "CIP_VERSION",
@@ -17,4 +18,7 @@ __all__ = [
     "LipIdentity",
     "LipSource",
     "build_lip_envelope",
+    "TrustFSM",
+    "TrustState",
+    "TrustTransition",
 ]
