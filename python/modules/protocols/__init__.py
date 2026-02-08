97d48d8faab876fe6d87bdd305fda721f315ed3f
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-02-08 10:45:37 +0300

    Add HCP and LIP protocol helpers

--- python/modules/protocols/__init__.py
+++ python/modules/protocols/__init__.py
@@ -1,1 +1,20 @@
 """Protocol utilities for CIP/HCP/LIP."""
+
+from .cip import CIP_VERSION, CipIdentity, CipState, build_envelope as build_cip_envelope
+from .hcp import HCP_VERSION, HcpHumanState, HcpIdentity, build_envelope as build_hcp_envelope
+from .lip import LIP_VERSION, LipIdentity, LipSource, build_envelope as build_lip_envelope
+
+__all__ = [
+    "CIP_VERSION",
+    "CipIdentity",
+    "CipState",
+    "build_cip_envelope",
+    "HCP_VERSION",
+    "HcpHumanState",
+    "HcpIdentity",
+    "build_hcp_envelope",
+    "LIP_VERSION",
+    "LipIdentity",
+    "LipSource",
+    "build_lip_envelope",
+]
