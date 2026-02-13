50d8fa16add86b212010bbec15262f64d6441386
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-02-13 11:33:17 +0300

    Add Web4 multi-transport abstraction scaffolding

--- python/modules/web4_runtime/__init__.py
+++ python/modules/web4_runtime/__init__.py
@@ -5,6 +5,9 @@ from .lip_runtime import LipRuntime
 from .observability import ObservabilityHub, ObservabilityEvent
 from .protocol_router import Web4ProtocolRouter
 from .rtt import RttConfig, RttSession, RttStats, BackpressureError, DisconnectedError
+from .transport import RttTransport, TransportBackend
+from .transport_registry import TransportRegistry
+from .web4_session import Web4Session
 
 __all__ = [
     "AgentLoopAdapter",
@@ -20,4 +23,8 @@ __all__ = [
     "RttStats",
     "BackpressureError",
     "DisconnectedError",
+    "Web4Session",
+    "TransportRegistry",
+    "TransportBackend",
+    "RttTransport",
 ]
