d1bad4d81cae69ffcca1543c9211ca217cd7f904
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-06-23 14:35:35 +0300

    feat: export CaPU adapter

--- python/modules/trusted_runtime/adapters/__init__.py
+++ python/modules/trusted_runtime/adapters/__init__.py
@@ -1,11 +1,14 @@
 """Optional adapters for the LS Trusted Runtime."""
 
+from .capu import CaPUConfig, CaPUExecutionAdapter
 from .cml import CMLConfig, CMLCausalAuditAdapter
 from .dao_lim import DAOlimConfig, DAOlimRoutingAdapter
 from .proofpath import ProofPathAuthorizationBundleAdapter, ProofPathConfig
 from .pythia import PythiaLabsConfig, PythiaLabsEvidenceAdapter
 
 __all__ = [
+    "CaPUConfig",
+    "CaPUExecutionAdapter",
     "CMLConfig",
     "CMLCausalAuditAdapter",
     "DAOlimConfig",
