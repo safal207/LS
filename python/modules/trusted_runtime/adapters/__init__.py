d32212ab29656bdcf77dbef9fd55b8953221cd78
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-06-23 11:24:32 +0300

    feat: export PythiaLabs and ProofPath adapters

--- python/modules/trusted_runtime/adapters/__init__.py
+++ python/modules/trusted_runtime/adapters/__init__.py
@@ -2,10 +2,16 @@
 
 from .cml import CMLConfig, CMLCausalAuditAdapter
 from .dao_lim import DAOlimConfig, DAOlimRoutingAdapter
+from .proofpath import ProofPathAuthorizationBundleAdapter, ProofPathConfig
+from .pythia import PythiaLabsConfig, PythiaLabsEvidenceAdapter
 
 __all__ = [
     "CMLConfig",
     "CMLCausalAuditAdapter",
     "DAOlimConfig",
     "DAOlimRoutingAdapter",
+    "ProofPathAuthorizationBundleAdapter",
+    "ProofPathConfig",
+    "PythiaLabsConfig",
+    "PythiaLabsEvidenceAdapter",
 ]
